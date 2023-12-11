35b7a4423c65bce929ba7e915530eea5a65b0fcd
Joseph <ralphlsulit@gmail.com>
2023-12-11 21:26:20 +0800

    feat: add header feature items from strapi

---
+++ frontend/assets/customStyles.js
@@ -0,0 +1,14 @@
+import styles from '../styles/customStyles.module.css';
+
+export const imageContainerStyles = [
+  styles['image-container-0'],
+  styles['image-container-1'],
+  styles['image-container-2'],
+  styles['span-1'],
+  styles['span-2'],
+];
+
+export const featureStyles = [
+  styles['header-feature'],
+  styles['header-feature-img1'],
+]
--- frontend/assets/imageStyles.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/imageStyles.module.css';
-
-export const imageContainerStyles = [
-    styles['image-container-0'],
-    styles['image-container-1'],
-    styles['image-container-2'],
-    styles['span-1'],
-    styles['span-2']
-  ];
